0c18e31d0f03f852b01670e738d7452964b24b68
ScottyPoi <kyzambo@gmail.com>
2021-03-23 14:13:19 -0600

    create gatsby site

---
+++ SimpleSerialize/src/pages/about.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const AboutPage = () => (
+  <main>
+    <h1>Scott Simpson</h1>
+    <a href='https://www.github.com/scottypoi'>@ScottyPoi</a>
+  </main>
+)
+export default AboutPage
