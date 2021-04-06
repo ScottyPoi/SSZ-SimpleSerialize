f57520008217e181268d59c80e8cf7d41d6a2eed
ScottyPoi <kyzambo@gmail.com>
2021-04-06 17:47:33 -0600

    move header to top of layout

--- sszapp/components/TopHeader.jsx
+++ sszapp/components/TopHeader.jsx
@@ -1,42 +1,60 @@
 import React from 'react';
 import * as ReactBootStrap from 'react-bootstrap';
+import Head from 'next/head'
 
 export default function TopHeader({...props}) {
-    return (
-        <ReactBootStrap.Col md={12}>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark" collapseonselect="true" expand="lg" bg="dark" variant="dark">
-                <div className='container-fluid'>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className='collapse navbar-collapse' id="navbarNavDropdown">       
-                        <a className='navbar-brand' href='./'>SSZ</a>
-                        <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
-                        {props.pages.map((page) => {
-                            if (!page.pages) {
-                            return (
-                                <li key={page.name} className='nav-item'>
-                                    <a className='nav-link' href={`#${page.name}`} >{page.name}</a>
-                                </li>
-                                )}
-                            else { return (
-                                <li key={page.name} className='nav-item dropdown'>
-                                    <a className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" id={page.name} aria-expanded='false' >{page.name}</a>
-                                    <ul className='dropdown-menu' aria-labelledby={page.name}>
-                                        {page.pages.map((subpage) => {
-                                            return (
-                                                <li key={subpage.name} ><a className='dropdown-item' href='#'> {subpage.name}</a></li>
-                                            )})}
-                                    </ul>
-                                </li>
-                                )}
-                            })
-                        }
-                        </ul>
+    return (<>
+            <Head>
+              <meta charSet="utf-8" />
+            </Head>
+            <Head>
+              <meta name="viewport" content="width=device-width, initial-scale=1" />
+            </Head>
+            <Head>
+              <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossOrigin="anonymous" />
+            </Head>
+            <Head>
+              <link href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" rel="stylesheet" />
+            </Head>
+            <Head> 
+              <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" /> 
+            </Head>
+            <ReactBootStrap.Col md={12}>
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark" collapseonselect="true" expand="lg" bg="dark" variant="dark">
+                    <div className='container-fluid'>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className='collapse navbar-collapse' id="navbarNavDropdown">       
+                            <a className='navbar-brand' href='./'>SSZ</a>
+                            <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
+                            {props.pages.map((page) => {
+                                if (!page.pages) {
+                                return (
+                                    <li key={page.name} className='nav-item'>
+                                        <a className='nav-link' href={`#${page.name}`} >{page.name}</a>
+                                    </li>
+                                    )}
+                                else { return (
+                                    <li key={page.name} className='nav-item dropdown'>
+                                        <a className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" id={page.name} aria-expanded='false' >{page.name}</a>
+                                        <ul className='dropdown-menu' aria-labelledby={page.name}>
+                                            {page.pages.map((subpage) => {
+                                                return (
+                                                    <li key={subpage.name} ><a className='dropdown-item' href='#'> {subpage.name}</a></li>
+                                                )})}
+                                        </ul>
+                                    </li>
+                                    )}
+                                })
+                            }
+                            </ul>
+                        </div>
                     </div>
-                </div>
-            </nav>
-        </ReactBootStrap.Col>
+                </nav>
+            </ReactBootStrap.Col>
+            </>
+
 
     )
 }
--- sszapp/components/layout.jsx
+++ sszapp/components/layout.jsx
@@ -22,31 +22,28 @@ const contentStyle = {
 export default function Layout({ children }) {
     return (
           <div className='Layout' style={layoutStyle}>
-            <Head>
-              <meta charSet="utf-8" />
-              <meta name="viewport" content="width=device-width, initial-scale=1" />
-              <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossOrigin="anonymous" />
-              <link href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" rel="stylesheet" />
-              <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" /> 
-            </Head>
-            <body>
+            <div className='row fixed-top'>
+              <div className='row'>
+                <TopHeader { ...MainPage.args } />
+              </div>
+
+              <div className='d-flex flex-row justify-content-between align-items-end'>
+
+                <div className='d-flex flex-column justify-content-center p-5'>Site Map</div>
+                <div className='d-flex flex-column justify-content-center p-5'>Table of Contents</div>
+              </div>
+
+            </div>
+            <body style={contentStyle}>
               <ReactBootStrap.Container fluid>
+                <div className='position-relative'>
+
                 <ReactBootStrap.Row>
-                  <ReactBootStrap.Col sm={2}>Site Map</ReactBootStrap.Col>
+                  <ReactBootStrap.Col sm={2}></ReactBootStrap.Col>
                   <ReactBootStrap.Col>{children}</ReactBootStrap.Col>
-                  <ReactBootStrap.Col sm={2}>Table of Contents</ReactBootStrap.Col>
+                  <ReactBootStrap.Col sm={2}></ReactBootStrap.Col>
                 </ReactBootStrap.Row>
-                <ReactBootStrap.Row className="fixed-top">
-                  <ReactBootStrap.Col>
-                    <ReactBootStrap.Row>
-                      <TopHeader { ...MainPage.args } />
-                    </ReactBootStrap.Row>
-                    <ReactBootStrap.Row>
-                      <ReactBootStrap.Col lg={8} />
-                      <ReactBootStrap.Col sm={4} />
-                    </ReactBootStrap.Row>
-                  </ReactBootStrap.Col>
-                </ReactBootStrap.Row>
+                </div>
               </ReactBootStrap.Container>
               <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossOrigin="anonymous"></script>
             </body>
@@ -54,3 +51,15 @@ export default function Layout({ children }) {
           )
   }
   
+
+  // <ReactBootStrap.Row className="fixed-top">
+  //                 <ReactBootStrap.Col>
+  //                   <ReactBootStrap.Row>
+  //                     <TopHeader { ...MainPage.args } />
+  //                   </ReactBootStrap.Row>
+  //                   <ReactBootStrap.Row>
+  //                     <ReactBootStrap.Col lg={8} />
+  //                     <ReactBootStrap.Col sm={4} />
+  //                   </ReactBootStrap.Row>
+  //                 </ReactBootStrap.Col>
+  //               </ReactBootStrap.Row>
