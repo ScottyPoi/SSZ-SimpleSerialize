01d8ad3df759d77d4b8d9a7be4e1e63f92d7858c
ScottyPoi <kyzambo@gmail.com>
2021-04-06 20:47:10 -0600

    edit site map

--- sszapp/components/NavBar.jsx
+++ sszapp/components/NavBar.jsx
@@ -1,14 +1,63 @@
+import React from 'react';
+import Link from 'next/link';
+import styles from './NavBar.module.css';
+
 const navBarStyle = {
     backgroundColor: "red",
     color: "white",
     width: "100%",
-    height: "60px"
+    height: "100%"
   };
   
-  const NavBar = () => (
-    <div className="NavBar" style={navBarStyle}>
-      NAVBAR
-    </div>
-  );
-  
-  export default NavBar;
+export default function NavBar({ ...props }) {
+
+    return (
+      <div >
+        <div className='nav flex-column' id='navbarSupportedContent'>
+          <div className='row justify-content-center'>Site Map</div>
+          <div className='row'>
+            <div className='d-flex flex-column'>
+          <ul className="navbar-nav">
+          {props.pages.map((page) => {
+            if (!page.pages) {
+              return (
+              <div className='d-flex flex-row'>
+                <li className='nav-link' href="#" key={page.name}>
+                    {page.name}  
+                </li>
+              </div>
+
+              )
+            }
+            else { return (
+              <div className='d-flex flex-row'>
+              <li key={page.name}>
+                <div className='row' >
+                  <Link className='nav-link' href='#'>{page.name}</Link>
+                      {page.pages.map((subpage) => {
+                        return (
+                        <div className='d-flex flex-row' key={subpage.name}>
+                          <li>
+                            <Link className={`nav-link`} href="#" style={{fontSize: 80}}>
+                              <div className={styles.sitemap3}>{subpage.name}</div>
+                            </Link>  
+                          </li>
+                        </div>
+                        )
+                      })}
+                  </div>       
+                </li>              
+              </div>
+            )}
+          })
+          }
+
+          </ul>
+          </div>
+          </div>
+
+          
+        </div>
+      </div>
+    );
+};
--- sszapp/components/layout.jsx
+++ sszapp/components/layout.jsx
@@ -4,6 +4,8 @@ import TopHeader from '../components/TopHeader';
 import { MainPage } from '../stories/TopHeader.stories';
 import NavBar from '../components/NavBar';
 import * as ReactBootStrap from 'react-bootstrap';
+import  pages  from '../data/site-pages.json';
+
 
 
 const layoutStyle = {
@@ -26,25 +28,26 @@ export default function Layout({ children }) {
               <div className='row'>
                 <TopHeader { ...MainPage.args } />
               </div>
+              <div className='d-flex row justify-content-between'>
+                <div className='d-flex col-3 justify-content-center'>
+                  <nav class="nav align-items-stretch flex-column navbar-dark bg-dark">
+                    <NavBar { ...MainPage.args } />
+                  </nav>
 
-              <div className='d-flex flex-row justify-content-between align-items-end'>
-
-                <div className='d-flex flex-column justify-content-center p-5'>Site Map</div>
-                <div className='d-flex flex-column justify-content-center p-5'>Table of Contents</div>
+                </div>
+                <div className='d-flex col-2 justify-content-center p-5'>Table of Contents</div>
               </div>
 
             </div>
             <body style={contentStyle}>
-              <ReactBootStrap.Container fluid>
-                <div className='position-relative'>
+                <div className='container position-relative'>
 
-                <ReactBootStrap.Row>
-                  <ReactBootStrap.Col sm={2}></ReactBootStrap.Col>
-                  <ReactBootStrap.Col>{children}</ReactBootStrap.Col>
-                  <ReactBootStrap.Col sm={2}></ReactBootStrap.Col>
-                </ReactBootStrap.Row>
+                <div className='d-flex flex-row'>
+                  <div className='col-2'></div>
+                  <div className='col-7'>{children}</div>
+                  <div className='col-3'></div>
                 </div>
-              </ReactBootStrap.Container>
+                </div>
               <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossOrigin="anonymous"></script>
             </body>
           </div>
@@ -52,14 +55,3 @@ export default function Layout({ children }) {
   }
   
 
-  // <ReactBootStrap.Row className="fixed-top">
-  //                 <ReactBootStrap.Col>
-  //                   <ReactBootStrap.Row>
-  //                     <TopHeader { ...MainPage.args } />
-  //                   </ReactBootStrap.Row>
-  //                   <ReactBootStrap.Row>
-  //                     <ReactBootStrap.Col lg={8} />
-  //                     <ReactBootStrap.Col sm={4} />
-  //                   </ReactBootStrap.Row>
-  //                 </ReactBootStrap.Col>
-  //               </ReactBootStrap.Row>
