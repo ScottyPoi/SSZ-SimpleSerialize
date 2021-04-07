b6cb7c2eebafa4696a4cacd0cd82fb1dd88ab0e9
ScottyPoi <kyzambo@gmail.com>
2021-04-07 17:46:10 -0600

    move <Head> to layout

--- sszapp/components/TopHeader.jsx
+++ sszapp/components/TopHeader.jsx
@@ -4,21 +4,6 @@ import Head from 'next/head'
 
 export default function TopHeader({...props}) {
     return (<>
-            <Head>
-              <meta charSet="utf-8" />
-            </Head>
-            <Head>
-              <meta name="viewport" content="width=device-width, initial-scale=1" />
-            </Head>
-            <Head>
-              <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossOrigin="anonymous" />
-            </Head>
-            <Head>
-              <link href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" rel="stylesheet" />
-            </Head>
-            <Head> 
-              <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" /> 
-            </Head>
             <ReactBootStrap.Col md={12}>
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark" collapseonselect="true" expand="lg" bg="dark" variant="dark">
                     <div className='container-fluid'>
--- sszapp/components/layout.jsx
+++ sszapp/components/layout.jsx
@@ -24,6 +24,21 @@ const contentStyle = {
 export default function Layout({ children }) {
     return (
           <div className='Layout' style={layoutStyle}>
+            <Head>
+              <meta charSet="utf-8" />
+            </Head>
+            <Head>
+              <meta name="viewport" content="width=device-width, initial-scale=1" />
+            </Head>
+            <Head>
+              <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossOrigin="anonymous" />
+            </Head>
+            <Head>
+              <link href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" rel="stylesheet" />
+            </Head>
+            <Head> 
+              <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" /> 
+            </Head>
             <div className='row fixed-top'>
               <div className='row'>
                 <TopHeader { ...MainPage.args } />
