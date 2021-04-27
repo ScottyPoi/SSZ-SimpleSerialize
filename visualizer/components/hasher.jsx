241e73ba2ddcca73d6b21735351748e13c4d2886
ScottyPoi <kyzambo@gmail.com>
2021-04-27 13:01:21 -0600

    Update hasher.jsx

--- visualizer/components/hasher.jsx
+++ visualizer/components/hasher.jsx
@@ -1,1 +1,10 @@
-TODO
+const {createHash} = require('crypto');
+const testData = "some test data"
+const hash = createHash('sha256')
+hash.update(testData);
+const same = createHash('sha256');
+same.update(testData)
+const encoded = hash.digest('hex');
+const sameEncoded = same.digest('hex');
+
+console.log(encoded == sameEncoded);
