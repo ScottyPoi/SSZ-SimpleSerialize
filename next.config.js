df088f3d6b2ad8e2e9e2add30b1330387425dbcb
ScottyPoi <66335769+ScottyPoi@users.noreply.github.com>
2021-05-13 15:14:35 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,10 +4,5 @@ module.exports = {
   future: {
     webpack5: true,
   },
-  images: {
-    loader: 'cloudinary',
-    path:'http://res.cloudinary.com/ssz/'
-  },
-  basePath: '/SSZ-Simpleserialize',
-  assetPrefix: '/SSZ-Simpleserialize/'
+
 }
