e1d3d3dda4afe87b62d341bcd522fb5f31bad412
ScottyPoi <kyzambo@gmail.com>
2021-05-13 10:52:52 -0600

    change image loader to cloudify

--- next.config.js
+++ next.config.js
@@ -1,25 +1,10 @@
-const images = require('remark-images')
-const emoji = require('remark-emoji')
-const toc = require('remark-toc');
-const gfm = require('remark-gfm');
-const slug = require('remark-slug')
-
-
-
-
-const withMDX = require('@next/mdx')({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [images, emoji, toc, gfm, slug]
-  }
-})
-
-module.exports = withMDX({
-  pageExtensions: ['js', 'jsx', 'md', 'mdx']
-})
 
 module.exports = {
   future: {
     webpack5: true,
+  },
+  images: {
+    loader: 'cloudinary',
+    domains: ["res.cloudinary.com"],
   },
 }
