549d27c810db98f3d33f05dc003d1681518942ca
ScottyPoi <kyzambo@gmail.com>
2021-05-18 18:53:54 -0600

    Update TopBar.jsx

--- components/sandbox/Components/TopBar.jsx
+++ components/sandbox/Components/TopBar.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Select from "./Select";
-import ChangeFunction from './ChangeFunction';
+import ChangeFunction from "./ChangeFunction";
 export default function TopBar({ language, selectfunction, children }) {
   return (
     <div className="list-reset flex flex-wrap items-center justify-between my-2">
       <div>
-      <Select {...language} />
+        <Select {...selectfunction}>{children}</Select>
       </div>
       <div>
-      <Select {...selectfunction}>{children}</Select>
+        <Select {...language} />
       </div>
     </div>
   );
