71d550a8329e6ebc03265aa05ad6f5ac51a2d5a8
ScottyPoi <kyzambo@gmail.com>
2021-06-18 13:25:55 -0600

    Update wellKnown.jsx

--- simulator/ssz/types/basic/wellKnown.jsx
+++ simulator/ssz/types/basic/wellKnown.jsx
@@ -1,1 +1,12 @@
-TODO
+import { NumberUintType } from "./uint";
+import { BooleanType } from "./boolean";
+
+export default function byteType() {
+  return <NumberUintType byteLength={1}></NumberUintType>;
+}
+export function number32Type() {
+  return <NumberUintType byteLength={4}></NumberUintType>;
+}
+export function booleanType() {
+  return <BooleanType />;
+}
