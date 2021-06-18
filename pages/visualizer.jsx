994b69922b9d17fcdf340e433c0f0aa84f834068
ScottyPoi <kyzambo@gmail.com>
2021-06-18 13:36:11 -0600

    Create visualizer.jsx

---
+++ pages/visualizer.jsx
@@ -0,0 +1,11 @@
+import Controls from '../simulator/controls/Controls';
+
+export default function Visualizer(props) {
+    return (
+        <div className='container'>
+            <div className='row'>
+                <Controls />
+            </div>
+        </div>
+    )
+}
