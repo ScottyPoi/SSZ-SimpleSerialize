45180d84b67b356a154ff49e09c0d4223ce42294
ScottyPoi <ScottyPoi@users.noreply.github.com>
2021-08-11 23:43:45 +0000

    Deploying to gh-pages from @ ScottyPoi/SSZ-SimpleSerialize@edc9e2eb5a57d184bdcc55082c780c7c80bd307d 🚀

---
+++ _next/static/chunks/react-syntax-highlighter_languages_refractor_ini.15d13bc3fa2229cb44d6.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[6495],{14970:function(t){"use strict";function n(t){t.languages.ini={comment:/^[ \t]*[;#].*$/m,selector:/^[ \t]*\[.*?\]/m,constant:/^[ \t]*[^\s=]+?(?=[ \t]*=)/m,"attr-value":{pattern:/=.*/,inside:{punctuation:/^[=]/}}}}t.exports=n,n.displayName="ini",n.aliases=[]}}]);
