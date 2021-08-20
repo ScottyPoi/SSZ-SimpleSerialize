ff755680d7000065d93b99f2d93f431de80cc576
ScottyPoi <kyzambo@gmail.com>
2021-08-19 20:06:27 -0600

    helper function page

--- components/Demo/HelperFunctions.jsx
+++ components/Demo/HelperFunctions.jsx
@@ -1,9 +1,16 @@
 import ReactMarkdown from "react-markdown";
+import DisplayFunctions from '../sandbox/DisplayFunctions';
 
 export default function HelperFunctions(props) {
     return (
         <div className='container'>
+            <div className='row'>
             <ReactMarkdown>{props.text}</ReactMarkdown>
+                </div>
+                <div className='row'>
+<DisplayFunctions />
+                </div>
+           
         </div>
     )
 }
--- components/sandbox/DisplayFunctions.jsx
+++ components/sandbox/DisplayFunctions.jsx
@@ -1,69 +1,59 @@
-import { CopyBlock, dracula } from "react-code-blocks";
+import { CopyBlock, dracula, github } from "react-code-blocks";
 import { useState, useEffect } from "react";
-import TopBar from './Components/TopBar';
+import TopBar from "./Components/TopBar";
 
-import * as helpers from './components';
+import * as helpers from "./components";
 
 export default function DisplayFunctions() {
   const [language, changeLanguage] = useState("python");
   const [funct, changeFunct] = useState(helpers.getpoweroftwoceil);
-  const [languageDemo, changeDemo] = useState(helpers.getpoweroftwoceil["python"]);
+  const [demoLanguage, changeDemoLanguage] = useState(
+    helpers.getpoweroftwoceil["python"]
+  );
   const [lineNumbers, toggleLineNumbers] = useState(true);
-  const [helpies, setHelpies] = useState(helpers)
-  const [funlabel, setfunlabel] = useState(helpies[funct])
-
-
+  const [helpies, setHelpies] = useState(helpers);
+  const [funlabel, setfunlabel] = useState(helpies[funct]);
 
   useEffect(() => {
-    changeDemo(funct["python"]);
+    changeDemoLanguage(funct["python"]);
   }, [funct]);
 
   function toggleFunction(event) {
-    changeFunct(helpers[event.target.value])
-    changeDemo(helpers[event.target.value]["python"])
+    changeFunct(helpers[event]);
+    changeDemoLanguage(helpers[event]["python"]);
   }
 
   console.log(helpers);
-  console.log(typeof helpers)
-  console.log(Object.keys(helpers))
-  console.log(Object.keys(funct))
-  console.log(typeof Object.keys(helpers)[3])
-  console.log(typeof Object.keys(funct)[1])
+  console.log(typeof helpers);
+  console.log(Object.keys(helpers));
+  console.log(Object.keys(funct));
+  console.log(typeof Object.keys(helpers)[3]);
+  console.log(typeof Object.keys(funct)[1]);
+
+  function handleLangChange(lang) {
+    changeDemoLanguage(funct[lang]);
+    changeLanguage(lang);
+  }
 
   return (
-    <div>
-      <TopBar
-        language={{
-          value: language,
-          onChange: (e) => {
-            changeDemo(funct[e.target.value]);
-            return changeLanguage(e.target.value);
-          },
-          options: Object.keys(funct).map((lang) => (
-            <option key={lang} value={lang}>
-              {lang}
-            </option>
-          )),
-        }}
-        selectfunction={{
-          value: helpies[funct],
-          onChange: (e) => {
-            return toggleFunction(e)
-          },
-          options: Object.keys(helpies).map((key) => (
-            <option key={key} value={key}>
-            {key}
-            </option>
-          )),
-        }}
-      ></TopBar>
-      <div> 
-
+    <div className="row row-cols-2">
+      <div className="col">
+        {" "}
+        <div className='btn-group-vertical'>
+        {Object.keys(helpies).map((func, idx) => {
+          return (
+              <button type="button"  onClick={() => toggleFunction(func)} className='btn border btn-light'>{func}</button>
+              );
+            })}
+            </div>
+      </div>
+      <div className="col border">
+        <h5>{}</h5>
         <CopyBlock
           language={language}
-          text={languageDemo}
+          text={demoLanguage}
           showLineNumbers={false}
-          theme={dracula}
+          theme={github}
           wrapLines={true}
           codeBlock
         />
